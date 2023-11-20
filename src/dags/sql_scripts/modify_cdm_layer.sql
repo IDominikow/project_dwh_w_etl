b7a971232fb65a5c58ea311bfaf667b6064f0b46
s-sushko <ssushko.aw@gmail.com>
2023-11-20 05:43:48 +0300

    Create modify_cdm_layer.sql

---
+++ src/dags/sql_scripts/modify_cdm_layer.sql
@@ -0,0 +1,23 @@
+CREATE TABLE cdm.dm_courier_ledger (
+	id serial4 NOT NULL,
+	courier_id int4 NOT NULL,
+	courier_name text NOT NULL,
+	settlement_year int4 NOT NULL,
+	settlement_month int4 NOT NULL,
+	orders_count int4 NOT NULL,
+	orders_total_sum numeric(15, 9) NOT NULL,
+	rate_avg numeric(2, 1) NOT NULL,
+	order_processing_fee numeric(15, 9) NOT NULL,
+	courier_order_sum numeric(15, 9) NOT NULL,
+	courier_tips_sum numeric(15, 9) NOT NULL,
+	courier_reward_sum numeric(15, 9) NOT NULL,
+	CONSTRAINT dm_courier_ledger_courier_order_sum_check CHECK ((courier_order_sum >= (0)::numeric)),
+	CONSTRAINT dm_courier_ledger_courier_reward_sum_check CHECK ((courier_reward_sum >= (0)::numeric)),
+	CONSTRAINT dm_courier_ledger_courier_tips_sum_check CHECK ((courier_tips_sum >= (0)::numeric)),
+	CONSTRAINT dm_courier_ledger_order_processing_fee_check CHECK ((order_processing_fee >= (0)::numeric)),
+	CONSTRAINT dm_courier_ledger_orders_count_check CHECK ((orders_count >= 0)),
+	CONSTRAINT dm_courier_ledger_orders_total_sum_check CHECK ((orders_total_sum >= (0)::numeric)),
+	CONSTRAINT dm_courier_ledger_primary_key PRIMARY KEY (id),
+	CONSTRAINT dm_courier_ledger_rate_avg_check CHECK (((rate_avg >= (0)::numeric) AND (rate_avg <= (5)::numeric))),
+	CONSTRAINT dm_courier_ledger_uindex UNIQUE (courier_id, courier_name, settlement_year, settlement_month)
+);
