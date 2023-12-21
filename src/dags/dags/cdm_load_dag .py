ba8e2e6c41367ad691b40a0f486eb07f456cc354
s-sushko <ssushko.aw@gmail.com>
2023-12-21 04:25:40 +0300

    Update cdm_load_dag .py

--- src/dags/dags/cdm_load_dag .py
+++ src/dags/dags/cdm_load_dag .py
@@ -2,8 +2,9 @@ import logging
 
 import pendulum
 from airflow.decorators import dag, task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from cdm_scripts.dm_settlement_report_loader import SettlementReportLoader
-from cdm_scripts.dm_courier_ledger_report_loader import CourierLedgerLoader
+#from cdm_scripts.dm_courier_ledger_report_loader import CourierLedgerLoader
 from lib import ConnectionBuilder
 
 log = logging.getLogger(__name__)
@@ -29,14 +30,14 @@ def cdm_load_dag():
         rest_loader = SettlementReportLoader(dwh_pg_connect, dwh_pg_connect, log)
         rest_loader.load_from_dds()  # Вызываем функцию, которая перельет данные.
 
-    @task(task_id="load_courier_ledger_report")
-    def load_courier_ledger_report():
-        # создаем экземпляр класса, в котором реализована логика.
-        rest_loader = CourierLedgerLoader(dwh_pg_connect, dwh_pg_connect, log)
-        rest_loader.load_from_dds()  # Вызываем функцию, которая перельет данные.
+
+    load_courier_ledger_report = PostgresOperator(
+    task_id="load_courier_ledger_report",
+    postgres_conn_id="PG_WAREHOUSE_CONNECTION",
+    sql="load_dm_courier_ledger.sql",
+)
 
     # Инициализируем объявленные таски.
-    load_courier_ledger_report = load_courier_ledger_report()
     load_settlement_report = load_settlement_report()
 
     # Далее задаем последовательность выполнения тасков.
